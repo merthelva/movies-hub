daded8190a7df77147babeda55a7a8b80ac09bfa
Mert Helvaci <55186332+merthelva@users.noreply.github.com>
2026-06-02 23:01:30 +0300

    add generic error type class

---
+++ src/common/classes/api-error.class.ts
@@ -0,0 +1,11 @@
+class ApiError extends Error {
+  status: number;
+
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
+    this.name = "ApiError";
+  }
+}
+
+export { ApiError };
