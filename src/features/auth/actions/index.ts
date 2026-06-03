0115b54fa14e63045c2ec29ee52bb73d00ec60aa
Mert Helvaci <55186332+merthelva@users.noreply.github.com>
2026-06-03 16:28:27 +0300

    get rid of `try/catch` blocks and return formatted response for server actions

--- src/features/auth/actions/index.ts
+++ src/features/auth/actions/index.ts
@@ -9,7 +9,6 @@ import type {
   RegisterCredentialsType,
   RegisterResponseType,
 } from "@/features/auth/types/actions.type";
-import { ApiError } from "@/common/classes/api-error.class";
 import type { GenericResponseType } from "@/common/types/generic-response.type";
 import { HttpStatusCodes } from "@/common/constants/http-status-codes.constant";
 import { serializeErrorMessage } from "@/common/utils/serialize-error-message.util";
@@ -26,169 +25,116 @@ const getCookieAccessToken = async (): Promise<
 
 const login = async (
   credentials: LoginCredentialsType,
-): Promise<LoginResponseType> => {
-  try {
-    const response = await fetch(`${API_BASE_URL}/auth/login`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(credentials),
-    });
-    const jsonResp =
-      (await response.json()) as GenericResponseType<LoginResponseType>;
-
-    if ("error" in jsonResp) {
-      throw new ApiError(
-        jsonResp.statusCode,
-        serializeErrorMessage(jsonResp.message),
-      );
-    }
-
-    const cookieStore = await cookies();
-    cookieStore.set("accessToken", jsonResp.accessToken, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "lax",
-      maxAge: 15 * 60,
-    });
-
-    return jsonResp;
-  } catch (error: unknown) {
-    if (error instanceof ApiError) {
-      // Re-throw custom ApiError so UI can capture both error.status and error.message
-      throw error;
-    }
-
-    throw new ApiError(
-      HttpStatusCodes.INTERNAL_SERVER_ERROR,
-      error instanceof Error
-        ? error.message
-        : "An error occurred while logging the user in",
-    );
+): Promise<GenericResponseType<LoginResponseType>> => {
+  const response = await fetch(`${API_BASE_URL}/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(credentials),
+  });
+  const jsonResp = await response.json();
+  if (jsonResp?.status === "error") {
+    return {
+      ...jsonResp,
+      message: serializeErrorMessage(jsonResp.message),
+    };
   }
+
+  const cookieStore = await cookies();
+  cookieStore.set("accessToken", jsonResp.accessToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax",
+    maxAge: 15 * 60,
+  });
+
+  return {
+    status: "success",
+    data: jsonResp,
+  };
 };
 
 const register = async (
   credentials: RegisterCredentialsType,
-): Promise<RegisterResponseType> => {
-  try {
-    const response = await fetch(`${API_BASE_URL}/auth/register`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(credentials),
-    });
-    const jsonResp =
-      (await response.json()) as GenericResponseType<RegisterResponseType>;
-
-    if ("error" in jsonResp) {
-      throw new ApiError(
-        jsonResp.statusCode,
-        serializeErrorMessage(jsonResp.message),
-      );
-    }
-
-    return jsonResp;
-  } catch (error: unknown) {
-    if (error instanceof ApiError) {
-      // Re-throw custom ApiError so UI can capture both error.status and error.message
-      throw error;
-    }
-
-    throw new ApiError(
-      HttpStatusCodes.INTERNAL_SERVER_ERROR,
-      error instanceof Error
-        ? error.message
-        : "An error occurred while registering the user",
-    );
+): Promise<GenericResponseType<RegisterResponseType>> => {
+  const response = await fetch(`${API_BASE_URL}/auth/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(credentials),
+  });
+  const jsonResp = await response.json();
+  if (jsonResp?.status === "error") {
+    return {
+      ...jsonResp,
+      message: serializeErrorMessage(jsonResp.message),
+    };
   }
+
+  return {
+    status: "success",
+    data: jsonResp,
+  };
 };
 
-const logout = async (): Promise<SuccessResponseType> => {
-  try {
-    const [cookieStore, token] = await getCookieAccessToken();
-
-    if (!token) {
-      throw new ApiError(
-        HttpStatusCodes.UNAUTHORIZED,
-        "User is not authenticated",
-      );
-    }
-
-    const response = await fetch(`${API_BASE_URL}/auth/logout`, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    });
-    const jsonResp =
-      (await response.json()) as GenericResponseType<SuccessResponseType>;
-
-    if ("error" in jsonResp) {
-      throw new ApiError(
-        jsonResp.statusCode,
-        serializeErrorMessage(jsonResp.message),
-      );
-    }
-
-    cookieStore.delete("accessToken");
-    return jsonResp;
-  } catch (error: unknown) {
-    if (error instanceof ApiError) {
-      // Re-throw custom ApiError so UI can capture both error.status and error.message
-      throw error;
-    }
-
-    throw new ApiError(
-      HttpStatusCodes.INTERNAL_SERVER_ERROR,
-      error instanceof Error
-        ? error.message
-        : "An error occurred while logging the user out",
-    );
+const logout = async (): Promise<GenericResponseType<SuccessResponseType>> => {
+  const [cookieStore, token] = await getCookieAccessToken();
+  if (!token) {
+    return {
+      status: "error",
+      code: HttpStatusCodes.UNAUTHORIZED,
+      message: "User is not authenticated",
+    };
   }
+
+  const response = await fetch(`${API_BASE_URL}/auth/logout`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
+  const jsonResp = await response.json();
+  if (jsonResp?.status === "error") {
+    return {
+      ...jsonResp,
+      message: serializeErrorMessage(jsonResp.message),
+    };
+  }
+
+  cookieStore.delete("accessToken");
+  return {
+    status: "success",
+    data: jsonResp,
+  };
 };
 
-const removeAccount = async (userId: number): Promise<SuccessResponseType> => {
-  try {
-    const [cookieStore, token] = await getCookieAccessToken();
-
-    if (!token) {
-      throw new ApiError(
-        HttpStatusCodes.UNAUTHORIZED,
-        "User is not authenticated",
-      );
-    }
-
-    const response = await fetch(`${API_BASE_URL}/auth/remove/${userId}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const jsonResp =
-      (await response.json()) as GenericResponseType<SuccessResponseType>;
-
-    if ("error" in jsonResp) {
-      throw new ApiError(
-        jsonResp.statusCode,
-        serializeErrorMessage(jsonResp.message),
-      );
-    }
-
-    cookieStore.delete("accessToken");
-    return jsonResp;
-  } catch (error: unknown) {
-    if (error instanceof ApiError) {
-      // Re-throw custom ApiError so UI can capture both error.status and error.message
-      throw error;
-    }
-
-    throw new ApiError(
-      HttpStatusCodes.INTERNAL_SERVER_ERROR,
-      error instanceof Error
-        ? error.message
-        : "An error occurred while removing the user account",
-    );
+const removeAccount = async (
+  userId: number,
+): Promise<GenericResponseType<SuccessResponseType>> => {
+  const [cookieStore, token] = await getCookieAccessToken();
+  if (!token) {
+    return {
+      status: "error",
+      code: HttpStatusCodes.UNAUTHORIZED,
+      message: "User is not authenticated",
+    };
   }
+
+  const response = await fetch(`${API_BASE_URL}/auth/remove/${userId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const jsonResp = await response.json();
+  if (jsonResp?.status === "error") {
+    return {
+      ...jsonResp,
+      message: serializeErrorMessage(jsonResp.message),
+    };
+  }
+
+  cookieStore.delete("accessToken");
+  return jsonResp;
 };
 
 const getCurrentUser = async (): Promise<AuthContextValueType["user"]> => {
