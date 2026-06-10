bdd529fb982affc8294d1cdb02ff0e9ee15233e6
Mert Helvaci <55186332+merthelva@users.noreply.github.com>
2026-06-10 19:32:50 +0300

    rename and extend util function

--- src/common/utils/serialize-error-message.util.ts
+++
@@ -1,8 +0,0 @@
-const serializeErrorMessage = (message: string | Array<string>): string => {
-  if (typeof message === "string") {
-    return message;
-  }
-  return message.map((msg) => `❌ ${msg}`).join("\n");
-};
-
-export { serializeErrorMessage };
---
+++ src/common/utils/serialize-message.util.ts
@@ -0,0 +1,34 @@
+import type { StateVariantType } from "../types/state-variant.type";
+
+const serializeMessage = (
+  variant: StateVariantType,
+  message: string | Array<string>,
+): string => {
+  let emoji: string | null = null;
+  switch (variant) {
+    case "info": {
+      emoji = "ℹ️";
+      break;
+    }
+    case "success": {
+      emoji = "✅";
+      break;
+    }
+    case "warning": {
+      emoji = "⚠️";
+      break;
+    }
+    case "error":
+    default: {
+      emoji = "❌";
+      break;
+    }
+  }
+
+  if (typeof message === "string") {
+    return `${emoji} ${message}`;
+  }
+  return message.map((msg) => `${emoji} ${msg}`).join("\n");
+};
+
+export { serializeMessage };
