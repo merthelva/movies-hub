507cabaad7f149ba14ed434c9e843fa492a52e51
Mert Helvaci <55186332+merthelva@users.noreply.github.com>
2026-06-19 01:14:37 +0300

    remove unnecessary comment block

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,15 +1,3 @@
-// import { getRequestConfig } from "next-intl/server";
-
-// export default getRequestConfig(async () => {
-//   // Static for now, we'll change this later
-//   const locale = "en";
-
-//   return {
-//     locale,
-//     messages: (await import(`../../messages/${locale}.json`)).default,
-//   };
-// });
-
 import { getRequestConfig } from "next-intl/server";
 import { hasLocale } from "next-intl";
 import { routing } from "./routing";
