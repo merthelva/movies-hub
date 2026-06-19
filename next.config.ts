214c3342b26217c8de00dd023ef0e8f9b4326943
Mert Helvaci <55186332+merthelva@users.noreply.github.com>
2026-06-19 22:46:55 +0300

    configure remote pattern for image fetch

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+    ],
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
