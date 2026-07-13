e60e856eac1fab8626c3ce7dc98f5642f7bf95c9
Mert Helvaci <55186332+merthelva@users.noreply.github.com>
2026-07-13 16:27:54 +0300

    remove unnecessary soft redirection upon successful login

--- src/app/[locale]/auth/login/page.tsx
+++ src/app/[locale]/auth/login/page.tsx
@@ -1,24 +1,9 @@
-import { redirect } from "@/i18n/navigation";
 import { AuthForm } from "@/features/auth/components/AuthForm";
 import { sanitizeRedirect } from "@/common/utils/sanitize-redirect.util";
-import { getCurrentUser } from "@/features/auth/actions";
-import { Language } from "@/common/constants/language.constant";
-import type { LocaleType } from "@/common/types/locale.type";
 
 export default async function LoginPage({
-  params,
   searchParams,
 }: PageProps<"/[locale]/auth/login">) {
-  const { locale } = await params;
-  const response = await getCurrentUser(Language[locale as LocaleType]);
-
-  if (response.ok) {
-    redirect({
-      href: "/",
-      locale,
-    });
-  }
-
   const { redirect: redirectParam } = await searchParams;
   const raw = Array.isArray(redirectParam) ? redirectParam[0] : redirectParam;
   const redirectTo = sanitizeRedirect(raw);
