1e48f28bc07ef04efe2deb03646221eb9e9ec850
Mert Helvaci <55186332+merthelva@users.noreply.github.com>
2026-07-20 10:20:00 +0300

    wrap logo by nav link

--- src/components/Header/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo.tsx
@@ -4,20 +4,22 @@ import Image from "next/image";
 
 import styles from "./styles.module.scss";
 
+import { NavLink } from "@/components/ui/NavLink";
 import { useTheme } from "@/hooks/useTheme.hook";
 
 const HeaderLogo = () => {
   const { theme } = useTheme();
 
   return (
-    <Image
-      src={`/logo-${theme}.png`}
-      alt="Movies Hub"
-      className={styles.logo}
-      width={theme === "dark" ? 108 : 112}
-      height={50}
-      priority
-    />
+    <NavLink className={styles.logo} href="/">
+      <Image
+        src={`/logo-${theme}.png`}
+        alt="Movies Hub"
+        width={theme === "dark" ? 108 : 112}
+        height={50}
+        priority
+      />
+    </NavLink>
   );
 };
 
